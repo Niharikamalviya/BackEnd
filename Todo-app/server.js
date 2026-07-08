f7daf8337f4168a43b2211478d3fb503486bf3e7
Niharikamalviya <neharikamalviya05@gmail.com>
2026-07-08 14:17:17 +0530

    todo-app

---
+++ Todo-app/server.js
@@ -0,0 +1,38 @@
+
+const express = require("express");
+const app = express();
+
+
+// load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//middleware to parse json request body
+
+app.use(express.json());
+
+//import routes for todo API 
+
+const todoRoutes = require("./routes/todos");
+
+//mount the todo ASPI routes
+
+app.use("/api/V1", todoRoutes);
+
+//start server
+
+app.listen(PORT, () => {
+    console.log(`server started successfully at ${PORT}`)
+})
+
+//connect to the database
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+
+//default route
+app.get("/", (req, res) => {
+    res.send(`<h1>this is home page</h1>`)
+})
+
