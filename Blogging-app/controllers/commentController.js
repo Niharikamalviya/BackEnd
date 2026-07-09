c52417ff18af650cbfdaca59b8e494a0170a4e43
Niharikamalviya <neharikamalviya05@gmail.com>
2026-07-10 01:23:28 +0530

    comments

--- Blogging-app/controllers/commentController.js
+++ Blogging-app/controllers/commentController.js
@@ -1,19 +1,33 @@
 const Post = require("../models/postModel");
-const Comment = require("../models/commentModel");
+const Comment = require("../models/commentsModels");
 
 exports.createComment = async (req, res) => {
     try {
         const { post, user, body } = req.body;
         const comment = new Comment({
-            post, user, bpdy
+            post, user, body
         });
 
         const savedComment = await comment.save();
 
+        //find post by ID add the new comment to its comments array
+        const updatedPost = await Post.findByIdAndUpdate(post, { $push: { comments: savedComment._id } }, { new: true })
+            .populate("comments")
+            .exec();
 
+        res.json({
+            post: updatedPost,
+        });
     }
     catch (err) {
+        console.error(err);
+        console.log(err);
+        res.status(500)
+            .json({
+                success: false,
+                data: "intrnal server error",
+                message: err.message,
+            })
 
     }
-
-}
+};
--- Blogging-app/routes/blog.js
+++ Blogging-app/routes/blog.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 //import
 const { createBlog } = require("../controllers/createblog");
+const { createComment } = require("../controllers/commentController");
+const { dummyLink } = require("../controllers/likeController");
 
 
 //API routes
 router.post("/createblog", createBlog);
+router.post("/comment/create", createComment);
+router.get("/dummyroute", dummyLink)
 
 
 module.exports = router;
