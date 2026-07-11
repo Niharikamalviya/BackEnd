168d96863dc26db05f111b01a2ce0c619473db40
Niharikamalviya <neharikamalviya05@gmail.com>
2026-07-11 18:24:07 +0530

    token-cookie

--- Auth-app/controller/Auth.js
+++ Auth-app/controller/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 const User = require("../models/user");
+require("dotenv").config();
 
 //signUp route handler
 
@@ -8,7 +10,7 @@ exports.signup = async (req, res) => {
         //get data
 
         const { name, email, password, role } = req.body
-        //check if user alredy exist
+        //check if user alredy exist to call DD by usinf findOne function 
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
@@ -58,6 +60,89 @@ exports.signup = async (req, res) => {
 
         })
 
+
+    }
+}
+
+//login
+
+exports.login = async (req, res) => {
+    try {
+        //data fetch 
+        const { email, password } = req.body;
+        //validation on email and password
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "please fill all the details carefully",
+            });
+        }
+
+        //check for registter user
+
+        const user = await User.findOne({ email });
+        //if not a register user
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                meassage: "user not found"
+            });
+        }
+
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        }
+
+        //verify password & generate a JWT token
+        if (await bcrypt.compare(password, user.password)) {
+            //password match
+            let token = jwt.sign(payload, process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h",
+
+                });
+
+            user.token = tekon;
+            user.password = undefined;
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: "user Logged in successfully"
+
+
+            });
+
+        }
+        else {
+            return res.status(403).json({
+                success: false,
+                message: "password Incorrect",
+
+
+            })
+        }
+
+
+
+
+
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Login failure",
+
+        })
 
     }
 }
