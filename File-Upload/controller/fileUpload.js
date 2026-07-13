b3c04d855a371f7c815e724c50fbe05b77c2d586
Niharikamalviya <neharikamalviya05@gmail.com>
2026-07-13 17:14:06 +0530

    gmail-send

--- File-Upload/controller/fileUpload.js
+++ File-Upload/controller/fileUpload.js
@@ -10,18 +10,22 @@ exports.localFileUpload = async (req, res) => {
         const file = req.files.file;
         console.log("File fetched", file);
 
+        //create path where file need to be stored on server
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
+        console.log("PATH->", path)
+
+        //add path to the move function
         file.mv(path, (err) => {
             console.log(err);
         });
+
+        //create a successfull response
         res.json({
             success: true,
             message: "Local file uploaded successfully",
 
 
         });
-
-
 
     }
     catch (error) {
--- File-Upload/models/File.js
+++ File-Upload/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
 const fileSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -14,9 +16,41 @@ const fileSchema = new mongoose.Schema({
 
     },
     email: {
-        type: String
+        type: String,
     },
 });
+
+//post middleware
+fileSchema.post("save", async function (doc) {
+    try {
+        console.log("DOC", doc)  //doc means the enrty created in your DB 
+
+        //transporter
+        //best practice to write into under config 
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                PASS: process.env.MAIL_PASSWORD,
+
+            },
+        });
+
+        //send mail
+        let info = await transporter.sendMail({
+            from: `codehelp`,
+            to: doc.email,
+            subject: "New file uploaded on cloudinary",
+            html: `<h2>Hello user </h2> <p>file upload successfully<p> view here <a href= "${doc.imageUrl}">${doc.imageUrl}</a>`
+        })
+        console.log("INFO", info);
+
+    }
+    catch (error) {
+        console.error(error);
+
+    }
+})
 
 const file = mongoose.model("File", fileSchema);
 module.exports = File;
